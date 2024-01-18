5a411e5614df7493d9d63dca681c5792599571aa
Prayag-09 <91512981+Prayag-09@users.noreply.github.com>
2024-01-18 21:21:54 +0530

    ZOD

--- backend src/index.js
+++ backend src/index.js
@@ -3,13 +3,15 @@ const app = express();
 app.use(express.json());
 const zod = require('zod');
 
-import { createTodo,updateTodo } from '../types';
+import { createTodo,updateTodo } from './types';
 app.post('/todos' , (req,res) => {
-    const {title,desc} = req.body;
-    const obj = {title,desc};
+    const obj = req.body;
     const validate = createTodo.safeParse(obj);
-    if(validate){
-        
+    if(!validate.success){
+        res.status(404).json({
+            message : "Wrong inputs. Try Again"
+        });
+        return;
     }
 })
 
@@ -20,8 +22,11 @@ app.put('/completed' , (req,res) => {
     const ID = req.body.id;
     const validate = updateTodo.safeParse(ID);
 
-    if(validate){
-
+    if(!validate.success){
+        res.status(404).json({
+            message : "Invalid ID"
+        })
+        return;
     }
 })
 app.listen(3000);
